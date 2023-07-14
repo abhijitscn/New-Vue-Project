6e80180e510426761f7af69ba1cf9bea1af950db
abhijitscn <abhijits@capitalnumbers.com>
2023-07-14 12:50:48 +0530

    add vuetify

--- src/main.js
+++ src/main.js
@@ -2,7 +2,16 @@ import { createApp } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
 import "./style.css";
 import App from "./App.vue";
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 import HomeVue from "./container/Home.vue";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
 const router = createRouter({
   history: createWebHistory(),
@@ -15,5 +24,6 @@ const router = createRouter({
 });
 
 const app = createApp(App);
+app.use(vuetify);
 app.use(router);
 app.mount("#app");
